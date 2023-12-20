f8d382e502e00a4e3824848cad197c090b09e001
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2023-12-20 09:49:13 -0600

    deploy with yaml

--- flow.py
+++ flow.py
@@ -1,14 +1,5 @@
 from prefect import flow
 
-
 @flow(log_prints=True)
 def hello():
-    print("Hello!")
-
-
-if __name__ == "__main__":
-    hello.deploy(
-        name="my-deployment",
-        work_pool_name="my-work-pool",
-        image="kevingrismoreprefect/cicd-example:latest",
-    )
+  print("Hello!")
